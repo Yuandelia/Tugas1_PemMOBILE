2aaa06020d535def1046183cab8d8588bf237447
Yuandelia <168386257+Yuandelia@users.noreply.github.com>
2024-12-15 14:13:54 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Essential Widget Project'),
+          backgroundColor: Colors.amber[700],
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.iconscout.com/icon/free/png-256/free-flutter-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-vol-3-pack-logos-icons-2944876.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
